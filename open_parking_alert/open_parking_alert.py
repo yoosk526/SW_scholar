e8e03bf3736054aa0846ce0a23baef7cbbcbc657
SeungKweon Yoo <yoosk526@naver.com>
2022-09-16 02:30:58 +0900

    Update open_parking_alert.py
    Timestamp및 잔오류 수정함.

--- open_parking_alert/open_parking_alert.py
+++ open_parking_alert/open_parking_alert.py
@@ -10,6 +10,9 @@ from pathlib import Path
 import requests
 import json
 
+# For timestamp
+import time
+
 url = "https://dev.rs-aiot.com/api/v1/integrations/http/f65b368b-370c-7f17-a97b-aa18d6b6ca44"
 # url = "http://localhost:3000/api/user"
 headers = {'Content-type': 'application/json; charset=utf-8'}
@@ -37,6 +40,9 @@ def get_car_boxes(boxes, class_ids):
 
     return np.array(car_boxes)
 
+# Variable for timestamp
+now = time
+
 
 # Root directory of the project
 ROOT_DIR = Path(".")
@@ -58,8 +64,7 @@ IMAGE_DIR = os.path.join(ROOT_DIR, "images")
 VIDEO_SOURCE = "parking_lot_5.mp4"
 
 # Create a Mask-RCNN model in inference mode
-model = MaskRCNN(mode="inference", model_dir=MODEL_DIR,
-                 config=MaskRCNNConfig())
+model = MaskRCNN(mode="inference", model_dir=MODEL_DIR, config=MaskRCNNConfig())
 
 # Load pre-trained model
 model.load_weights(COCO_MODEL_PATH, by_name=True)
@@ -119,26 +124,31 @@ while video_capture.isOpened():
         for box in parked_car_boxes:
             print("Car: ", box)
         print(" parked_ Array Dimension = ", len(parked_car_boxes))
+
         # make a comparison array for checking free space
         comparison = np.zeros((len(parked_car_boxes)), dtype='bool')
 
+        # array for storing timestamp of each parking lot area
+        park_in = np.full((len(parked_car_boxes)), int(now.time()), dtype='int')
+        park_out = np.zeros((len(parked_car_boxes)), dtype='int')
+
         data = {
-            "deviceName": "kk-A-Park\n",
+            "deviceName": "kk-B-Park\n",
             "deviceType": "IDLEPARK\n",
-            "timestamp": 1662094009000,
+            "timestamp": int(now.time()),
             "payload": {
-                "A1": {"isfree": "False", "inTime": 1662094009000, "outTime": 0},
-                "A2": {"isfree": "False", "inTime": 1662094009000, "outTime": 0},
-                "A3": {"isfree": "False", "inTime": 1662094009000, "outTime": 0},
-                "A4": {"isfree": "False", "inTime": 1662094009000, "outTime": 0},
-                "A5": {"isfree": "False", "inTime": 1662094009000, "outTime": 0},
-                "A6": {"isfree": "False", "inTime": 1662094009000, "outTime": 0},
-                "A7": {"isfree": "False", "inTime": 1662094009000, "outTime": 0},
-                "A8": {"isfree": "False", "inTime": 1662094009000, "outTime": 0},
-                "A9": {"isfree": "False", "inTime": 1662094009000, "outTime": 0},
-                "A10": {"isfree": "False", "inTime": 1662094009000, "outTime": 0},
-                "A11": {"isfree": "False", "inTime": 1662094009000, "outTime": 0},
-                "A12": {"isfree": "False", "inTime": 1662094009000, "outTime": 0}
+                "A1": {"isfree": "False", "inTime": int(park_in[0]), "outTime": int(park_out[0])},
+                "A2": {"isfree": "False", "inTime": int(park_in[1]), "outTime": int(park_out[1])},
+                "A3": {"isfree": "False", "inTime": int(park_in[2]), "outTime": int(park_out[2])},
+                "A4": {"isfree": "False", "inTime": int(park_in[3]), "outTime": int(park_out[3])},
+                "A5": {"isfree": "False", "inTime": int(park_in[4]), "outTime": int(park_out[4])},
+                "A6": {"isfree": "False", "inTime": int(park_in[5]), "outTime": int(park_out[5])},
+                "A7": {"isfree": "False", "inTime": int(park_in[6]), "outTime": int(park_out[6])},
+                "A8": {"isfree": "False", "inTime": int(park_in[7]), "outTime": int(park_out[7])},
+                "A9": {"isfree": "False", "inTime": int(park_in[8]), "outTime": int(park_out[8])},
+                "A10": {"isfree": "False", "inTime": int(park_in[9]), "outTime": int(park_out[9])},
+                "A11": {"isfree": "False", "inTime": int(park_in[10]), "outTime": int(park_out[10])},
+                "A12": {"isfree": "False", "inTime": int(park_in[11]), "outTime": int(park_out[11])}
             }
         }
         res = requests.post(url, headers=headers, data=json.dumps(data))
@@ -153,7 +163,6 @@ while video_capture.isOpened():
         print("Array Dimension = ", len(car_boxes))
 
         # See how much those cars overlap with the known parking spaces
-
         overlaps = mrcnn.utils.compute_overlaps(parked_car_boxes, car_boxes)
 
         # Assume no spaces are free until we find one that is free
@@ -172,12 +181,13 @@ while video_capture.isOpened():
             y1, x1, y2, x2 = parking_area
 
             # Check if the parking space is occupied by seeing if any car overlaps
-            # it by more than 0.15 using IoU
-            if max_IoU_overlap < 0.15:
+            # it by more than 0.20 using IoU
+            if max_IoU_overlap < 0.40:
                 # Parking space not occupied! Draw a green box around it
                 cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 3)
                 # Flag that we have seen at least one open space
                 free_space[i] = True
+                park_out[i] = int(now.time())
                 i += 1
                 # free_space = True
             else:
@@ -186,25 +196,39 @@ while video_capture.isOpened():
 
             # Write the IoU measurement inside the box
             font = cv2.FONT_HERSHEY_DUPLEX
-            cv2.putText(frame, f"{max_IoU_overlap:0.2}",
-                        (x1 + 6, y2 - 6), font, 0.3, (255, 255, 255))
+            cv2.putText(frame, f"{max_IoU_overlap:0.2}", (x1 + 6, y2 - 6), font, 0.3, (255, 255, 255))
+        
+        print("주차 가능 공간 : ", free_space)
+
         data = {
-            "deviceName": "kk-A-Park\n",
+            "deviceName": "kk-B-Park\n",
             "deviceType": "IDLEPARK\n",
-            "timestamp": 1662094009000,
+            "timestamp": int(now.time()),
             "payload": {
-                "A1": {"isfree": np.array2string(free_space[0]), "inTime": 1662094009000, "outTime": 0},
-                "A2": {"isfree": np.array2string(free_space[1]), "inTime": 1662094009000, "outTime": 0},
-                "A3": {"isfree": np.array2string(free_space[2]), "inTime": 1662094009000, "outTime": 0},
-                "A4": {"isfree": np.array2string(free_space[3]), "inTime": 1662094009000, "outTime": 0},
-                "A5": {"isfree": np.array2string(free_space[4]), "inTime": 1662094009000, "outTime": 0},
-                "A6": {"isfree": np.array2string(free_space[5]), "inTime": 1662094009000, "outTime": 0},
-                "A7": {"isfree": np.array2string(free_space[6]), "inTime": 1662094009000, "outTime": 0},
-                "A8": {"isfree": np.array2string(free_space[7]), "inTime": 1662094009000, "outTime": 0},
-                "A9": {"isfree": np.array2string(free_space[8]), "inTime": 1662094009000, "outTime": 0},
-                "A10": {"isfree": np.array2string(free_space[9]), "inTime": 1662094009000, "outTime": 0},
-                "A11": {"isfree": np.array2string(free_space[10]), "inTime": 1662094009000, "outTime": 0},
-                "A12": {"isfree": np.array2string(free_space[11]), "inTime": 1662094009000, "outTime": 0}
+                # "A1": {"isfree": np.array2string(free_space[3]), "inTime": 1662094009000, "outTime": 0},
+                # "A2": {"isfree": np.array2string(free_space[0]), "inTime": 1662094009000, "outTime": 0},
+                # "A3": {"isfree": np.array2string(free_space[4]), "inTime": 1662094009000, "outTime": 0},
+                # "A4": {"isfree": np.array2string(free_space[5]), "inTime": 1662094009000, "outTime": 0},
+                # "A5": {"isfree": np.array2string(free_space[2]), "inTime": 1662094009000, "outTime": 0},
+                # "A6": {"isfree": np.array2string(free_space[1]), "inTime": 1662094009000, "outTime": 0},
+                # "A7": {"isfree": np.array2string(free_space[6]), "inTime": 1662094009000, "outTime": 0},
+                # "A8": {"isfree": np.array2string(free_space[7]), "inTime": 1662094009000, "outTime": 0},
+                # "A9": {"isfree": np.array2string(free_space[8]), "inTime": 1662094009000, "outTime": 0},
+                # "A10": {"isfree": np.array2string(free_space[9]), "inTime": 1662094009000, "outTime": 0},
+                # "A11": {"isfree": np.array2string(free_space[10]), "inTime": 1662094009000, "outTime": 0},
+                # "A12": {"isfree": np.array2string(free_space[11]), "inTime": 1662094009000, "outTime": 0}
+                "A1": {"isfree": np.array2string(free_space[0]), "inTime": int(park_in[0]), "outTime": int(park_out[0])},
+                "A2": {"isfree": np.array2string(free_space[1]), "inTime": int(park_in[1]), "outTime": int(park_out[1])},
+                "A3": {"isfree": np.array2string(free_space[2]), "inTime": int(park_in[2]), "outTime": int(park_out[2])},
+                "A4": {"isfree": np.array2string(free_space[3]), "inTime": int(park_in[3]), "outTime": int(park_out[3])},
+                "A5": {"isfree": np.array2string(free_space[4]), "inTime": int(park_in[4]), "outTime": int(park_out[4])},
+                "A6": {"isfree": np.array2string(free_space[5]), "inTime": int(park_in[5]), "outTime": int(park_out[5])},
+                "A7": {"isfree": np.array2string(free_space[6]), "inTime": int(park_in[6]), "outTime": int(park_out[6])},
+                "A8": {"isfree": np.array2string(free_space[7]), "inTime": int(park_in[7]), "outTime": int(park_out[7])},
+                "A9": {"isfree": np.array2string(free_space[8]), "inTime": int(park_in[8]), "outTime": int(park_out[8])},
+                "A10": {"isfree": np.array2string(free_space[9]), "inTime": int(park_in[9]), "outTime": int(park_out[9])},
+                "A11": {"isfree": np.array2string(free_space[10]), "inTime": int(park_in[10]), "outTime": int(park_out[10])},
+                "A12": {"isfree": np.array2string(free_space[11]), "inTime": int(park_in[11]), "outTime": int(park_out[11])}
             }
         }
         res = requests.post(url, headers=headers, data=json.dumps(data))
@@ -226,11 +250,10 @@ while video_capture.isOpened():
         '''
 
         # # If a space has been free for several frames, we are pretty sure it is really free!
-        # if free_space_frames > 4:
-        #     # Write SPACE AVAILABLE!! at the top of the screen
-        #     font = cv2.FONT_HERSHEY_DUPLEX
-        #     cv2.putText(frame, f"SPACE AVAILABLE!", (10, 150),
-        #                 font, 3.0, (0, 255, 0), 2, cv2.FILLED)
+        if free_space_frames > 4:
+            # Write SPACE AVAILABLE!! at the top of the screen
+            font = cv2.FONT_HERSHEY_DUPLEX
+            cv2.putText(frame, f"SPACE AVAILABLE!", (10, 150), font, 3.0, (0, 255, 0), 2, cv2.FILLED)
 
         # Show the frame of video on the screen
         cv2.imshow('Video', frame)
