4746ff9e50d76f889748299a01427e862afb12f7
znyi <xinyi2709@gmail.com>
2022-09-17 00:39:03 +0900

    Update open_parking_alert.py
    class ParkingSpace //works, but parked_car_boxes is still used
    list ParkingSpaces //where I store other data about a particular parking space
    function postAll //to post all parking spaces to server
    function isFreeAll //to get the 'isFree' for all parking spaces, in the from of a list
    function checkIsFreeChange //to detect if there is a change in the state of parking space, not complete yet
    
    //codes that modify the parking space data (after the first frame) is a mess because I am still figuring out a way to identify which box is for which parking space

--- open_parking_alert/open_parking_alert.py
+++ open_parking_alert/open_parking_alert.py
@@ -1,3 +1,4 @@
+from operator import truediv
 import os
 import numpy as np
 import cv2
@@ -5,21 +6,79 @@ import mrcnn.config
 import mrcnn.utils
 from mrcnn.model import MaskRCNN
 from pathlib import Path
+# from twilio.rest import Client
 
+#from PIL import Image
 # For server++++++++++++++++++++
 import requests
 import json
 
-# For timestamp
+#time
 import time
 
+#for server
 url = "https://dev.rs-aiot.com/api/v1/integrations/http/f65b368b-370c-7f17-a97b-aa18d6b6ca44"
 # url = "http://localhost:3000/api/user"
-headers = {'Content-type': 'application/json; charset=utf-8'}
+
+# parking space management
+class ParkingSpace:
+    def __init__(self, box, now):
+        self.coord_box = box
+        self.inTime = now
+    
+    isFree = False
+    outTime = 0
+
+    def showjson(self):
+        return {"isFree": self.isFree, "inTime": self.inTime, "outTime": self.outTime}
+
+ParkingSpaces = [] #initial parking spaces detected are going to be the overall parking spaces available for parking, not yet addable, 고정이라고 가정
+
+def postAll(now):
+    pload = {}
+    for i in range(len(ParkingSpaces)):
+        pload['A'+str(i+1)] = ParkingSpaces[i].showjson()
+    headers = {'Content-type': 'application/json; charset=utf-8'}
+    data = {
+            "deviceName": "kk-X-Park\n", #"kk-B-Park\n",
+            "deviceType": "IDLEPARK\n",
+            "timestamp": now,
+            "payload": pload
+        }
+    print(data) #test
+    res = requests.post(url, headers=headers, data=json.dumps(data))
+ 
+def getIsFreeAll():
+    isFreeAll = list(map(lambda space: space.isFree, ParkingSpaces))
+    return isFreeAll
+
+def checkIsFreeChange(overlaps): #ret = 0 if no change, 1 if free space increase, -1 if free space decrease
+    #print(getIsFreeAll())
+    for i, parking_area, overlap_areas in zip(range(len(ParkingSpaces)), parked_car_boxes, overlaps):
+
+        ret = 0
+        max_IoU_overlap = np.max(overlap_areas)
+
+        # top-left and bottom-right coordinates of the parking area
+        y1, x1, y2, x2 = parking_area
+
+        if max_IoU_overlap > 0.6: 
+            # Parking space occupied
+            if(ParkingSpaces[i].isFree):
+                ParkingSpaces[i].isFree = False
+                ret = -1
+        else:
+            if(not ParkingSpaces[i].isFree):
+                ParkingSpaces[i].isFree = True
+                ret = 1
+    if(ret != 0):
+        print(getIsFreeAll())
+
+    return ret
+
+
 
 # Configuration that will be used by the Mask-RCNN library
-
-
 class MaskRCNNConfig(mrcnn.config.Config):
     NAME = "coco_pretrained_model_config"
     IMAGES_PER_GPU = 1
@@ -27,9 +86,8 @@ class MaskRCNNConfig(mrcnn.config.Config):
     NUM_CLASSES = 1 + 80  # COCO dataset has 80 classes + one background class
     DETECTION_MIN_CONFIDENCE = 0.6
 
+
 # Filter a list of Mask R-CNN detection results to get only the detected cars / trucks
-
-
 def get_car_boxes(boxes, class_ids):
     car_boxes = []
 
@@ -40,8 +98,13 @@ def get_car_boxes(boxes, class_ids):
 
     return np.array(car_boxes)
 
-# Variable for timestamp
-now = time
+
+# Twilio config
+# twilio_account_sid = 'YOUR_TWILIO_SID'
+# twilio_auth_token = 'YOUR_TWILIO_AUTH_TOKEN'
+# twilio_phone_number = 'YOUR_TWILIO_SOURCE_PHONE_NUMBER'
+# destination_phone_number = 'THE_PHONE_NUMBER_TO_TEXT'
+# client = Client(twilio_account_sid, twilio_auth_token)
 
 
 # Root directory of the project
@@ -62,6 +125,8 @@ IMAGE_DIR = os.path.join(ROOT_DIR, "images")
 
 # Video file or camera to process - set this to 0 to use your webcam instead of a video file
 VIDEO_SOURCE = "parking_lot_5.mp4"
+#"src_vid.mp4"#0
+# "test_images/parking.mp4"
 
 # Create a Mask-RCNN model in inference mode
 model = MaskRCNN(mode="inference", model_dir=MODEL_DIR, config=MaskRCNNConfig())
@@ -94,6 +159,9 @@ video_capture = cv2.VideoCapture(VIDEO_SOURCE)
 # How many frames of video we've seen in a row with a parking space open
 free_space_frames = 0
 
+# Have we sent an SMS alert yet?
+#sms_sent = False
+
 # Loop over each frame of video
 while video_capture.isOpened():
     success, frame = video_capture.read()
@@ -119,39 +187,19 @@ while video_capture.isOpened():
     if parked_car_boxes is None or len(parked_car_boxes) == 0:
         # This is the first frame of video - assume all the cars detected are in parking spaces.
         # Save the location of each car as a parking space box and go to the next frame of video.
-
+        
         parked_car_boxes = get_car_boxes(r['rois'], r['class_ids'])
+
+        now = round(time.time())
         for box in parked_car_boxes:
+            #pspace = ParkingSpace(box, now) #new parking space
+            #ParkingSpaces.append(pspace)
+            #print(ParkingSpaces[len(ParkingSpaces)-1].coord_box, ParkingSpaces[len(ParkingSpaces)-1].showjson()) #test
             print("Car: ", box)
-        print(" parked_ Array Dimension = ", len(parked_car_boxes))
+        ParkingSpaces = list(map(lambda box: ParkingSpace(box, now), parked_car_boxes))
+        postAll(now)
 
-        # make a comparison array for checking free space
-        comparison = np.zeros((len(parked_car_boxes)), dtype='bool')
-
-        # array for storing timestamp of each parking lot area
-        park_in = np.full((len(parked_car_boxes)), int(now.time()), dtype='int')
-        park_out = np.zeros((len(parked_car_boxes)), dtype='int')
-
-        data = {
-            "deviceName": "kk-B-Park\n",
-            "deviceType": "IDLEPARK\n",
-            "timestamp": int(now.time()),
-            "payload": {
-                "A1": {"isfree": "False", "inTime": int(park_in[0]), "outTime": int(park_out[0])},
-                "A2": {"isfree": "False", "inTime": int(park_in[1]), "outTime": int(park_out[1])},
-                "A3": {"isfree": "False", "inTime": int(park_in[2]), "outTime": int(park_out[2])},
-                "A4": {"isfree": "False", "inTime": int(park_in[3]), "outTime": int(park_out[3])},
-                "A5": {"isfree": "False", "inTime": int(park_in[4]), "outTime": int(park_out[4])},
-                "A6": {"isfree": "False", "inTime": int(park_in[5]), "outTime": int(park_out[5])},
-                "A7": {"isfree": "False", "inTime": int(park_in[6]), "outTime": int(park_out[6])},
-                "A8": {"isfree": "False", "inTime": int(park_in[7]), "outTime": int(park_out[7])},
-                "A9": {"isfree": "False", "inTime": int(park_in[8]), "outTime": int(park_out[8])},
-                "A10": {"isfree": "False", "inTime": int(park_in[9]), "outTime": int(park_out[9])},
-                "A11": {"isfree": "False", "inTime": int(park_in[10]), "outTime": int(park_out[10])},
-                "A12": {"isfree": "False", "inTime": int(park_in[11]), "outTime": int(park_out[11])}
-            }
-        }
-        res = requests.post(url, headers=headers, data=json.dumps(data))
+        print(" parked_ Array Dimension = ",len(parked_car_boxes))
     else:
         # We already know where the parking spaces are. Check if any are currently unoccupied.
 
@@ -160,16 +208,16 @@ while video_capture.isOpened():
 
         for box in car_boxes:
             print("Car: ", box)
-        print("Array Dimension = ", len(car_boxes))
 
+        print("Array Dimension = ",len(car_boxes))
+        
         # See how much those cars overlap with the known parking spaces
+        
         overlaps = mrcnn.utils.compute_overlaps(parked_car_boxes, car_boxes)
 
         # Assume no spaces are free until we find one that is free
-        free_space = np.zeros((len(parked_car_boxes)), dtype='bool')
-        # free_space = False
+        free_space = False
 
-        i = 0
         # Loop through each known parking space box
         for parking_area, overlap_areas in zip(parked_car_boxes, overlaps):
 
@@ -181,16 +229,12 @@ while video_capture.isOpened():
             y1, x1, y2, x2 = parking_area
 
             # Check if the parking space is occupied by seeing if any car overlaps
-            # it by more than 0.20 using IoU
-            if max_IoU_overlap < 0.40:
+            # it by more than 0.15 using IoU
+            if max_IoU_overlap < 0.15: #they changed the 0.15 to 0.4
                 # Parking space not occupied! Draw a green box around it
                 cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 3)
                 # Flag that we have seen at least one open space
-                free_space[i] = True
-                if park_out[i] == 0:    # Not updated value
-                    park_out[i] = int(now.time())
-                i += 1
-                # free_space = True
+                free_space = True
             else:
                 # Parking space is still occupied - draw a red box around it
                 cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 1)
@@ -198,63 +242,33 @@ while video_capture.isOpened():
             # Write the IoU measurement inside the box
             font = cv2.FONT_HERSHEY_DUPLEX
             cv2.putText(frame, f"{max_IoU_overlap:0.2}", (x1 + 6, y2 - 6), font, 0.3, (255, 255, 255))
-        
-        print("주차 가능 공간 : ", free_space)
-
-        data = {
-            "deviceName": "kk-B-Park\n",
-            "deviceType": "IDLEPARK\n",
-            "timestamp": int(now.time()),
-            "payload": {
-                # "A1": {"isfree": np.array2string(free_space[3]), "inTime": 1662094009000, "outTime": 0},
-                # "A2": {"isfree": np.array2string(free_space[0]), "inTime": 1662094009000, "outTime": 0},
-                # "A3": {"isfree": np.array2string(free_space[4]), "inTime": 1662094009000, "outTime": 0},
-                # "A4": {"isfree": np.array2string(free_space[5]), "inTime": 1662094009000, "outTime": 0},
-                # "A5": {"isfree": np.array2string(free_space[2]), "inTime": 1662094009000, "outTime": 0},
-                # "A6": {"isfree": np.array2string(free_space[1]), "inTime": 1662094009000, "outTime": 0},
-                # "A7": {"isfree": np.array2string(free_space[6]), "inTime": 1662094009000, "outTime": 0},
-                # "A8": {"isfree": np.array2string(free_space[7]), "inTime": 1662094009000, "outTime": 0},
-                # "A9": {"isfree": np.array2string(free_space[8]), "inTime": 1662094009000, "outTime": 0},
-                # "A10": {"isfree": np.array2string(free_space[9]), "inTime": 1662094009000, "outTime": 0},
-                # "A11": {"isfree": np.array2string(free_space[10]), "inTime": 1662094009000, "outTime": 0},
-                # "A12": {"isfree": np.array2string(free_space[11]), "inTime": 1662094009000, "outTime": 0}
-                "A1": {"isfree": np.array2string(free_space[0]), "inTime": int(park_in[0]), "outTime": int(park_out[0])},
-                "A2": {"isfree": np.array2string(free_space[1]), "inTime": int(park_in[1]), "outTime": int(park_out[1])},
-                "A3": {"isfree": np.array2string(free_space[2]), "inTime": int(park_in[2]), "outTime": int(park_out[2])},
-                "A4": {"isfree": np.array2string(free_space[3]), "inTime": int(park_in[3]), "outTime": int(park_out[3])},
-                "A5": {"isfree": np.array2string(free_space[4]), "inTime": int(park_in[4]), "outTime": int(park_out[4])},
-                "A6": {"isfree": np.array2string(free_space[5]), "inTime": int(park_in[5]), "outTime": int(park_out[5])},
-                "A7": {"isfree": np.array2string(free_space[6]), "inTime": int(park_in[6]), "outTime": int(park_out[6])},
-                "A8": {"isfree": np.array2string(free_space[7]), "inTime": int(park_in[7]), "outTime": int(park_out[7])},
-                "A9": {"isfree": np.array2string(free_space[8]), "inTime": int(park_in[8]), "outTime": int(park_out[8])},
-                "A10": {"isfree": np.array2string(free_space[9]), "inTime": int(park_in[9]), "outTime": int(park_out[9])},
-                "A11": {"isfree": np.array2string(free_space[10]), "inTime": int(park_in[10]), "outTime": int(park_out[10])},
-                "A12": {"isfree": np.array2string(free_space[11]), "inTime": int(park_in[11]), "outTime": int(park_out[11])}
-            }
-        }
-        res = requests.post(url, headers=headers, data=json.dumps(data))
+            
+            checkIsFreeChange(overlaps)
 
         # If at least one space was free, start counting frames
         # This is so we don't alert based on one frame of a spot being open.
         # This helps prevent the script triggered on one bad detection.
-        if np.all(free_space == comparison):
-            free_space_frames = 0
-        else:
-            free_space_frames += 1
-
-        '''
         if free_space:
             free_space_frames += 1
         else:
             # If no spots are free, reset the count
             free_space_frames = 0
-        '''
 
-        # # If a space has been free for several frames, we are pretty sure it is really free!
-        if free_space_frames > 4:
+        # If a space has been free for several frames, we are pretty sure it is really free!
+        if free_space_frames > 10:
             # Write SPACE AVAILABLE!! at the top of the screen
             font = cv2.FONT_HERSHEY_DUPLEX
             cv2.putText(frame, f"SPACE AVAILABLE!", (10, 150), font, 3.0, (0, 255, 0), 2, cv2.FILLED)
+
+            # If we haven't sent an SMS yet, sent it!
+            # if not sms_sent:
+            #     print("SENDING SMS!!!")
+            #     # message = client.messages.create(
+            #     #     body="Parking space open - go go go!",
+            #     #     from_=twilio_phone_number,
+            #     #     to=destination_phone_number
+            #     # )
+            #     sms_sent = True
 
         # Show the frame of video on the screen
         cv2.imshow('Video', frame)
